bc3bc3adf14ece7a843dcbda7f72e681d7feed16
Darien Márquez <darien1005@hotmail.com>
2022-12-12 09:16:04 -0700

    gh pages despliegue 1

--- android/app/src/main/java/ppm/gps/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package ppm.gps.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/app/models/models.ts
+++
@@ -1,9 +0,0 @@
-export interface Escuela{
-    nombre: string;
-    municipio: string;
-    clave: string;
-    nivel: string;
-}
-
-// comentario para ver si subve los cambios
-//ayuda no sirve mi push
